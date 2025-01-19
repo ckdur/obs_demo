90307683991fbbf7a8c964ee07abb8b4bfbeeb84
Ckristian Duran <ckdur.iso@gmail.com>
2025-01-19 12:02:10 +0900

    Bump of degate. New size for figures in sky130

--- lib/sky130_settings.py
+++ lib/sky130_settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Constraints
-pxpermicron = 15 # Pixeles per micron
+pxpermicron = 30 # Pixeles per micron
 metal_cont_names = ["li1.drawing - 67/20", "licon1.drawing - 66/44"] # Name (in .lyp). This will be displayed on metal captures
 diff_poly_names = ["diff.drawing - 65/20", "poly.drawing - 66/20"] # Name (in .lyp). This will be displayed on metal captures
 size_power = [0.36, 0.36] # The size of power in bottom [0] and top [1]
